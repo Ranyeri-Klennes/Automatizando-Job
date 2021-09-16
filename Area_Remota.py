8567dff40413118e3ee46bfb999ff569097e8530
ranyeri-klennes <ranyeri_klennes@hotmail.com>
2021-09-16 19:21:09 -0300

    Iniciando RPA para meu job atual...

---
+++ Area_Remota.py
@@ -0,0 +1,40 @@
+import pyautogui
+from time import sleep
+
+pyautogui.PAUSE = 0.5
+
+# DEIXANDO TUDO PRONTO
+
+# ABRIR WPP
+# pyautogui.press('win')
+# pyautogui.write('whatsapp', interval=0.2)
+# pyautogui.press('enter')
+
+# # ABRIR E-MAIL
+# pyautogui.press('win')
+# pyautogui.write('thunderbird', interval=0.2)
+# pyautogui.press('enter')
+
+# ABRIR NAVEGADOR / ACESSAR YOUTUBE
+pyautogui.press('win')
+pyautogui.write('firefox', interval=0.1)
+pyautogui.click(482, 372, 1, button='left')
+
+pyautogui.sleep(2)
+pergunta = pyautogui.confirm('Olá Rany, posso continuar?', buttons=['Sim','Não'])
+print(pergunta)
+if pergunta == 'Sim':
+    pyautogui.write('youtu.be')
+    pyautogui.press('enter')
+    pyautogui.sleep(10)
+    pyautogui.click(1266, 143, 1, interval=0.3, button='left')
+    if pyautogui.press('enter') == pyautogui.press('enter', pause=2) :
+        pyautogui.alert('Acessando conta...')
+else: 
+    pergunta2 = pyautogui.confirm('Assim que tiver ok avise...', buttons=['Ok','Parar!'])
+    if pergunta2 == 'Ok':
+        ... #faça tal coisa...
+    else:
+        pyautogui.FAILSAFE
+
+# ABRIR SISTEMA REMOTO
