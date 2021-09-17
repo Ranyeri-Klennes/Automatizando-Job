0e7960d09e25e5168395578b6f3c0c4c0e008958
ranyeri-klennes <ranyeri_klennes@hotmail.com>
2021-09-17 17:47:36 -0300

    - Tentei tornar um comando pyautogui como variavel não consegui. - Estou usando o comando mesmo para decisões.

--- Area_Remota.py
+++ Area_Remota.py
@@ -1,13 +1,10 @@
-from tkinter import LabelFrame
-import pyautogui
-from time import sleep
-# from pynput import keyboard
+import pyautogui, time
 
-pyautogui.PAUSE = 0.5
+pyautogui.PAUSE = 1
 
 # DEIXANDO TUDO PRONTO
 
-# ABRIR WPP
+# # ABRIR WPP
 # pyautogui.press('win')
 # pyautogui.write('whatsapp', interval=0.2)
 # pyautogui.press('enter')
@@ -23,24 +20,28 @@ pyautogui.write('firefox', interval=0.1)
 pyautogui.click(482, 372, 1, button='left')
 
 pyautogui.sleep(2)
-pergunta = pyautogui.confirm('Olá Rany, posso continuar?', buttons=['Sim','Não'])
-print(pergunta)
-if pergunta == 'Sim':
-    pyautogui.write('youtu.be')
-    pyautogui.press('enter')
-    pyautogui.sleep(10)
-    pyautogui.click(1266, 143, 1, interval=0.3, button='left')# clica para acessar o login.
-else: 
-    pyautogui.PAUSE
-
-
-# if pyautogui.press (['enter','enter']):
-#         pyautogui.alert('Acessando conta...')
-# else:
-#     pergunta2 = pyautogui.confirm('Assim que tiver ok avise...', buttons=['Ok','Parar!'])
-#     if pergunta2 == 'Ok':   
-#         ... #faça tal coisa...
-#     else:
-#         pyautogui.FailSafeException
+pyautogui.hotkey('ctrl','l')
+pyautogui.write('youtu.be')
+pyautogui.press('enter')
+pyautogui.sleep(10)
+pyautogui.click(1266, 143, 1, interval=0.3, button='left')# clica para acessar o login.
 
 # ABRIR SISTEMA REMOTO
+pergunta = pyautogui.confirm('Rany, posso continuar?', buttons=['Sim','Não'])
+if pergunta == 'Sim':
+    pyautogui.press('win')
+    pyautogui.write('remota', interval=0.2)
+    pyautogui.press('enter')
+    time.sleep(1)
+else:
+    pyautogui.PAUSE
+pyautogui.confirm('Rany, posso continuar?', buttons=['Sim','Não'])
+if pergunta == 'Sim':
+    pyautogui.sleep(15)
+    pyautogui.click(621, 435, 1, button='left')
+    pyautogui.write('gcf')
+    pyautogui.press('enter')
+    time.sleep(10)
+    pyautogui.click(621, 435, 1, button='left')
+else: 
+    pyautogui.sleep()
